0fd605177f9b2e9eac973409d8ff0a6c4bc84be0
y19th <destounchannel@gmail.com>
2023-02-25 14:46:11 +0300

    ver 1.3 recyclerview like card, in process overriding swipe behaviour

--- app/src/main/java/com/example/dailybruh/adapters/NewsPageRecyclerAdapter.kt
+++ app/src/main/java/com/example/dailybruh/adapters/NewsPageRecyclerAdapter.kt
@@ -3,15 +3,25 @@ package com.example.dailybruh.adapters
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.example.dailybruh.interfaces.ItemTouchHelperAdapter
 import com.example.dailybruh.databinding.RecyclerItemNewsPageBinding
+import com.example.dailybruh.dataclasses.Article
 import com.example.dailybruh.dataclasses.News
+import java.util.*
 
-class NewsPageRecyclerAdapter(private val news: News): RecyclerView.Adapter<NewsPageRecyclerAdapter.ViewHolder>() {
+class NewsPageRecyclerAdapter(private val news: News): RecyclerView.Adapter<NewsPageRecyclerAdapter.ViewHolder>(),
+    ItemTouchHelperAdapter {
+
+    init {
+        for(i in 0..itemCount) {
+          //  _items.add(news.articles[i])
+        }
+    }
 
     private lateinit var binding: RecyclerItemNewsPageBinding
     private lateinit var context: Context
+    private lateinit var _items: MutableList<Article>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         binding = RecyclerItemNewsPageBinding.inflate(LayoutInflater.from(parent.context),parent,false)
@@ -23,14 +33,19 @@ class NewsPageRecyclerAdapter(private val news: News): RecyclerView.Adapter<News
         binding.apply {
             holder.apply {
                 titlePage.text = news.articles[position].title
+                publishedatPage.text = "Опубликована: ${news.articles[position].time}"
+                authorPage.text = "Автор: ${news.articles[position].author}"
                 bindImage(urlPhoto,news.articles[position].image)
                 descPage.text = news.articles[position].desc
             }
         }
     }
 
-    override fun getItemCount(): Int = 5
+    override fun onItemMove(from: Int, to: Int) {
+        Collections.swap(_items,from,to)
+    }
 
+    override fun getItemCount(): Int = 5
 
     inner class ViewHolder: RecyclerView.ViewHolder(binding.root)
 }
---
+++ app/src/main/java/com/example/dailybruh/interfaces/ItemTouchHelperAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.dailybruh.interfaces
+
+interface ItemTouchHelperAdapter {
+    fun onItemMove(from:Int,to:Int)
+}
